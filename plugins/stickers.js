076597fd0fd3489b4b2ac99f1a39663a6a59e08b
chirutheyi <88527773+chiruthey@users.noreply.github.com>
2021-09-12 12:14:50 +0530

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -69,7 +69,7 @@ else if (Config.WORKTYPE == 'public') {
         //return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 
         }
-
+       else if (message.reply_message.image === false && message.reply_message.video) {
         ffmpeg(location)
             .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 600x600"])
             .videoFilters('scale=600:600:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=600:600:(ow-iw)/2:(oh-ih)/2:color=#00000000,setsar=1')
@@ -77,6 +77,7 @@ else if (Config.WORKTYPE == 'public') {
             .on('end', async () => {
                 await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
             });
+       }
         //return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
 }

216c72968d9c677209fd269400673cf52eeb606c
chirutheyi <88527773+chiruthey@users.noreply.github.com>
2021-09-15 10:16:43 +0530

    Update moddroid.js

--- plugins/moddroid.js
+++ plugins/moddroid.js
@@ -11,7 +11,9 @@ const { fetchJson, getBuffer } = require('./fetcher')
 const Language = require('../language')
 const Lang = Language.getString('search')
 
-Asena.addCommand({ pattern: 'mod ?(.*)', fromMe: false, desc: "Finds mod verssion." },  (async (message, match) => {
+if (Config.WORKTYPE == 'private') {
+    
+Asena.addCommand({ pattern: 'mod ?(.*)', fromMe: true, desc: "Finds mod verssion." },  (async (message, match) => {
     if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a app name.```', MessageType.text, { quoted: message.data });
     var reply = await message.client.sendMessage(message.jid, '```searching```', MessageType.text, {quoted: message.data});
     
@@ -27,3 +29,25 @@ Asena.addCommand({ pattern: 'mod ?(.*)', fromMe: false, desc: "Finds mod verssio
    await message.client.sendMessage(message.jid,ini_txt,MessageType.text, {quoted: message.data });  
    
 }));   
+}
+
+else if (Config.WORKTYPE == 'public') {
+    
+    Asena.addCommand({ pattern: 'mod ?(.*)', fromMe: false, desc: "Finds mod verssion." },  (async (message, match) => {
+    if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a app name.```', MessageType.text, { quoted: message.data });
+    var reply = await message.client.sendMessage(message.jid, '```searching```', MessageType.text, {quoted: message.data});
+    
+    get_result = await fetchJson(`https://api.lolhuman.xyz/api/moddroid?apikey=https://github.com/BlackAmda&query=${match[1]}`)
+                    get_result = get_result.result
+                    ini_txt = "" 
+                    for (var x of get_result) {
+                        ini_txt += `Name : ${x.name}\n`
+                        ini_txt += `Link : ${x.link}\n\n` 
+    
+        }
+    
+   await message.client.sendMessage(message.jid,ini_txt,MessageType.text, {quoted: message.data });  
+   
+}));   
+}
+    

82139c99d793ab4862ab3d1209a64e01826b6b12
chirutheyi <88527773+chiruthey@users.noreply.github.com>
2021-09-15 10:20:45 +0530

    Update joke.js

--- plugins/joke.js
+++ plugins/joke.js
@@ -5,7 +5,9 @@ const got = require('got');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
-Asena.addCommand({pattern: 'joke ?(.*)', fromMe: false, desc: Lang.JOKE_DESC}, async (message, match) => {
+if (Config.WORKTYPE == 'private') {
+    
+Asena.addCommand({pattern: 'joke ?(.*)', fromMe: true, desc: Lang.JOKE_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://official-joke-api.appspot.com/random_joke`;
 	try {
@@ -16,4 +18,19 @@ Asena.addCommand({pattern: 'joke ?(.*)', fromMe: false, desc: Lang.JOKE_DESC}, a
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 	}
-});
+}});
+	
+else if (Config.WORKTYPE == 'public') {
+	
+	Asena.addCommand({pattern: 'joke ?(.*)', fromMe: false, desc: Lang.JOKE_DESC}, async (message, match) => {
+	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
+	const url = `https://official-joke-api.appspot.com/random_joke`;
+	try {
+		const response = await got(url);
+		const json = JSON.parse(response.body);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🗣️ ' + Lang.JOKE +'* ```' + json.setup + '```\n\n' +
+		'*😆' + Lang.PUNCHLINE +'* ```' + json.punchline+ '```\n', MessageType.text);
+	} catch {
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
+	}
+}});

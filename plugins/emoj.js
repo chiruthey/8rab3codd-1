24bf7a3be1ace4e4a2e378820a572c7a0ad683bb
chirutheyi <88527773+chiruthey@users.noreply.github.com>
2021-09-15 10:13:52 +0530

    Update emoj.js

--- plugins/emoj.js
+++ plugins/emoj.js
@@ -1,11 +1,3 @@
-/* # Exclusively from danuma project 
-# Do not use this fore any commercial thing
-# If you abuse thais bot we wil kick you from bot 
-# Do not edit (Respect to the Devaoloper) 
-# All rights reserved ©Lasiya @lasiya99X t.me/lasiya99X
-# Get more about devaoloper https://lasiya.ml
-*/
-
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');

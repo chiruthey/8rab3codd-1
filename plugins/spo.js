1901162efb330c102dbed9910de7aec5b6c00e32
chirutheyi <88527773+chiruthey@users.noreply.github.com>
2021-09-22 21:26:08 -0700

    Update spo.js

--- plugins/spo.js
+++ plugins/spo.js
@@ -6,16 +6,16 @@ const axios = require('axios');
 const { errorMessage, infoMessage } = require('../helpers');
 const Config = require('../config');
 
-if (Config.WORKTYPE == 'private'){
+let wk = Config.WORKTYPE = 'public' ? false:true
         
-Asena.addCommand({ pattern: 'spotify ?(.*)', fromMe: true, desc: "Download song from spotify" }, (async (message, match) => {
+Asena.addCommand({ pattern: 'spotify ?(.*)', fromMe: wk, desc: "Download song from spotify" }, (async (message, match) => {
 
         const link = match[1]
     
          if (!link) return await message.client.sendMessage(message.jid, '```Give me a link```',MessageType.text)
     
         await axios
-          .get(`https://api.lolhuman.xyz/api/spotify?apikey=https://github.com/BlackAmda&url=${link}`)
+          .get(`https://api.lolhuman.xyz/api/spotify?apikey=6d8f65061357712bad246857=${link}`)
           .then(async (response) => {
             const {
               link,
@@ -29,30 +29,4 @@ Asena.addCommand({ pattern: 'spotify ?(.*)', fromMe: true, desc: "Download song 
             await message.client.sendMessage(message.jid,Buffer.from(profileBuffer.data), MessageType.document, {filename: title + '.mp3', mimetype: 'audio/mpeg'});
         });
  }));
-}
-
-else if (Config.WORKTYPE == 'public') {
-
-        Asena.addCommand({ pattern: 'spotify ?(.*)', fromMe: false, desc: "Download song from spotify" }, (async (message, match) => {
-
-        const link = match[1]
-    
-         if (!link) return await message.client.sendMessage(message.jid, '```Give me a link```',MessageType.text)
-    
-        await axios
-          .get(`https://api.lolhuman.xyz/api/spotify?apikey=https://github.com/BlackAmda&url=${link}`)
-          .then(async (response) => {
-            const {
-              link,
-              title,
-            } = response.data.result
-            
-            await message.client.sendMessage(message.jid, '```Downloading pls wait.```', MessageType.text);
-            const profileBuffer = await axios.get(link, {responseType: 'arraybuffer'})
-    
-            await message.client.sendMessage(message.jid, '```UPLOADING..```', MessageType.text);
-            await message.client.sendMessage(message.jid,Buffer.from(profileBuffer.data), MessageType.document, {filename: title + '.mp3', mimetype: 'audio/mpeg'});
-        });
- }));
-}
         
